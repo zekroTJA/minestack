c0c14d5c6ed07e31c9e82bea94f9b897e6c18388
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2024-03-12 22:47:05 +0100

    add minecraft version select

--- init.sh
+++ init.sh
@@ -23,7 +23,7 @@ setup_rclone_config() {
     echo -e "https://rclone.org/commands/rclone_config"
     echo -e ""
     echo -e "${F_ITALIC}Press ENTER to continue ...${F_RESET}"
-    read
+    read -r
 
     if ! [ -d "rclone" ]; then
         mkdir rclone
@@ -45,7 +45,7 @@ setup_rclone_config() {
 check_domain_binding() {
     domains=("$@")
 
-    set -- $(hostname -I)
+    set -- "$(hostname -I)"
     myip="$1"
 
     erroneous=()
@@ -60,7 +60,7 @@ check_domain_binding() {
         fi
     done
 
-    if [ -n "$erroneous" ]; then
+    if [ -n "${erroneous[*]}" ]; then
         echo -e "\n${F_BOLD}${C_ORANGE}warning:${F_RESET} some domains don't bind to this servers IP address ${F_ITALIC}${C_CYAN}($myip)${F_RESET}:"
         for err in "${erroneous[@]}"; do
             echo -e "  - $err"
@@ -68,7 +68,7 @@ check_domain_binding() {
 
         echo -e "\nDo you want to continue anyway? ${F_ITALIC}(y/N)${F_RESET}"
         echo -e "⚠️  Continuing with unset domain values can result in issues when issuing the TLS certificates for the public websites on startup."
-        read yn
+        read -r yn
         case "$yn" in
             "y"|"Y"|"yes") ;;
             *) exit 1 ;;
@@ -100,6 +100,32 @@ insert_backup_cronjob() {
     echo -e "\n${C_GREEN}Restart crontab entry has been created (${F_ITALIC}${output_dir}${F_RESET}${C_GREEN}).${F_RESET}"
 }
 
+select_minecraft_version() {
+    latest_version=$(curl -Ls https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.release')
+
+    if which ffzf > /dev/null 2>&1; then
+        selected_version=$(curl -Ls https://launchermeta.mojang.com/mc/game/version_manifest.json \
+            | jq -r '.versions[] | select(.type == "release") | .id' \
+            | fzf --border rounded --header "Please select a Minecraft version for your server:" --info hidden)
+    else
+        echo -e "\nPlease enter the version of Minecraft you want to use ${F_ITALIC}(press enter to use latest ($latest_version))${F_RESET}:"
+        read -r selected_version
+    fi
+
+    if [ -z "$selected_version" ]; then
+        selected_version=$latest_version
+    fi
+
+    if [ "$(curl -so /dev/null -w "%{response_code}" https://hub.spigotmc.org/versions/$selected_version.json)" == "200" ]; then
+        echo -e "\n✅ ${C_GREEN}Spigot build for selected version exists!${F_RESET}"
+    else
+        error "No spigot build exists for the selected Minecraft version. Maybe you want to select an older version."
+        exit 1
+    fi
+
+    export minecraft_version=$selected_version
+}
+
 error() {
     echo -e "${C_RED}${F_BOLD}error:${F_RESET} $1"
 }
@@ -107,14 +133,14 @@ error() {
 # -------------------------------------------------------------------------
 
 missing=()
-which curl > /dev/null 2>&1 || missing+=("docker")
+which docker > /dev/null 2>&1 || missing+=("docker")
 which curl > /dev/null 2>&1 || missing+=("curl")
 which jq > /dev/null 2>&1 || missing+=("jq")
 which dig > /dev/null 2>&1 || missing+=("dig")
 which sudo > /dev/null 2>&1 || missing+=("sudo")
-if [ -n "$missing" ]; then
+if [ -n "${missing[*]}" ]; then
     error "The following tools are missing on your system:"
-    for t in ${missing[*]}; do
+    for t in "${missing[@]}"; do
         echo -e "  - $t"
     done
     echo -e ""
@@ -126,8 +152,8 @@ set -e
 
 echo -e "${F_BOLD}${C_CYAN}Heyo! 👋 This script will lead you though the necessary steps to set up your Minecraft server!${F_RESET} 🚀"
 
-echo -e "\n${C_GREY}(1)${F_RESET} Please enter the ${F_BOLD}${C_PINK}domain${F_RESET} of your server ${F_ITALIC}(i.e. 'mc.example.com')${F_RESET}:"
-read domain
+echo -e "\nPlease enter the ${F_BOLD}${C_PINK}domain${F_RESET} of your server ${F_ITALIC}(i.e. 'mc.example.com')${F_RESET}:"
+read -r domain
 
 if [ -z "$domain" ]; then
     error "Value for domain can not be empty!"
@@ -136,10 +162,14 @@ fi
 
 check_domain_binding "$domain" "docker.$domain" "grafana.$domain"
 
-echo -e "ROOT_DOMAIN=$domain" > ".env"
+select_minecraft_version
 
-echo -e "\n${C_GREY}(2)${F_RESET} Do you want to enable ${F_BOLT}${C_PINK}automatic backups${F_RESET}? ${F_ITALIC}(Y/n)${F_RESET}"
-read yn
+{   echo -e "ROOT_DOMAIN=$domain"
+    echo -e "MINECRAFT_VERSION=$minecraft_version"
+} > ".env"
+
+echo -e "\nDo you want to enable ${F_BOLD}${C_PINK}automatic backups${F_RESET}? ${F_ITALIC}(Y/n)${F_RESET}"
+read -r yn
 case "$yn" in
     "n"|"N"|"no") echo -e "Automatic backups are not enabled." ;;
     *) setup_rclone_config ;;
@@ -147,38 +177,40 @@ esac
 
 echo -e "\n Downloading required plugins:"
 echo -e "  - Downloading latest version of BlueMap from GitHub ..."
+# shellcheck disable=SC2046
 set -- $(curl -Ls https://api.github.com/repos/BlueMap-Minecraft/BlueMap/releases \
     | jq -r '[ .[] | select ( .prerelease == false ) ][0].assets[] | select ( .name | endswith ("-spigot.jar" ) ) | .browser_download_url + " " + .name')
 curl -Lso "spigot/plugins/$2" "$1"
 
 echo -e "  - Downloading latest version of minecraft-prometheus-exporter from GitHub ..."
+# shellcheck disable=SC2046
 set -- $(curl -Ls https://api.github.com/repos/sladkoff/minecraft-prometheus-exporter/releases \
     | jq -r '[ .[] | select ( .prerelease == false ) ][0].assets[] | select ( .name | endswith (".jar") ) | .browser_download_url + " " + .name')
 curl -Lso "spigot/plugins/$2" "$1"
 
-echo -e "\n${C_GREY}(3)${F_RESET} Do you want to enable ${F_BOLT}${C_PINK}automatic restarts${F_RESET}? ${F_ITALIC}(Y/n)${F_RESET}"
+echo -e "\nDo you want to enable ${F_BOLD}${C_PINK}automatic restarts${F_RESET}? ${F_ITALIC}(Y/n)${F_RESET}"
 echo -e "⚠️  This is required to perform automatic backups, because backups can only be created on server startup."
-read yn
+read -r yn
 case "$yn" in
     "n"|"N"|"no") echo -e "Automatic backups are not enabled." ;;
     *) insert_backup_cronjob ;;
 esac
 
-echo -e "\n${C_GREY}(4)${F_RESET} Do you want to start the stack now ${F_RESET}? ${F_ITALIC}(Y/n)${F_RESET}"
-read yn
+echo -e "\nDo you want to start the stack now? ${F_ITALIC}(Y/n)${F_RESET}"
+read -r yn
 case "$yn" in
     "n"|"N"|"no")
-        echo -e "${C_GREEN}Setup finished!${F_RESET}"
+        echo -e "\n${C_GREEN}Setup finished!${F_RESET}"
         echo -e "You can start the stack at any time with ${F_ITALIC}'docker compose up -d'${F_RESET}! ;)"
         ;;
     *)
         docker compose up -d
-        echo -e "${C_GREEN}Stack has been started!${F_RESET}"
+        echo -e "\n${C_GREEN}Stack has been started!${F_RESET}"
         echo -e ""
         echo -e "Next steps:"
         echo -e "  - Check stack status with ${F_ITALIC}'docker compose ps'${F_RESET} and ${F_ITALIC}'docker compose logs'${F_RESET}."
-        echo -e "  - Go to ${F_UNERLINE}${C_CYAN}https://docker.$domain${F_RESET} to configure Portainer."
-        echo -e "  - Go to ${F_UNERLINE}${C_CYAN}https://$domain${F_RESET} to check your BlueMap (when the server has finished building)."
+        echo -e "  - Go to ${F_UNDERLINE}${C_CYAN}https://docker.$domain${F_RESET} to configure Portainer."
+        echo -e "  - Go to ${F_UNDERLINE}${C_CYAN}https://$domain${F_RESET} to check your BlueMap (when the server has finished building)."
         echo -e "  - Connect to your server via ${C_CYAN}$domain${F_RESET}."
         echo -e "  - Have fun! :)"
         ;;
